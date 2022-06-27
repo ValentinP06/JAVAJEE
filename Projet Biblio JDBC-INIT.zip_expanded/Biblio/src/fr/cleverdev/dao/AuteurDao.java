c6fe56d5e3fa65e62973dea233073e3b63c8e30f
Valentin Payet <valentinpayet06@gmail.com>
2022-06-27 18:57:13 +0200

    cours et tp du 27 Juin

---
+++ Projet Biblio JDBC-INIT.zip_expanded/Biblio/src/fr/cleverdev/dao/AuteurDao.java
@@ -0,0 +1,18 @@
+package fr.cleverdev.dao;
+
+import java.util.List;
+
+import fr.cleverdev.model.Auteur;
+
+public interface AuteurDao {
+
+	void         creer( Auteur auteur ) throws DaoException;
+
+    Auteur       trouver( long id ) throws DaoException;
+
+    List<Auteur> lister() throws DaoException;
+
+    void         supprimer( long id ) throws DaoException;
+    
+    void 		 modifier (long id, Auteur auteur) throws DaoException;  
+}
---
+++ TestJEE/src/main/java/testservlet/test.java
@@ -0,0 +1,28 @@
+package testservlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class test
+ */
+
+public class test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher("/WEB-INF/HelloJSP.jsp").forward(request, response);
+	}
+
+
+	}
+
